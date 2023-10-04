2798a8e547cdb69f004b12d46cc6190b25fc45f2
sophuang <109146037+sophuang@users.noreply.github.com>
2023-10-04 16:34:14 +0800

    Score showing on the side

--- src/components/HeartRating.vue
+++ src/components/HeartRating.vue
@@ -4,15 +4,19 @@
     <div v-for="(rate, index) in ratings" :key="index" class="mr-1">
       <button @click="toggleHeart(index)">
         <emptyHeart v-if="rate === 0" />
-        <halfHeart v-if="rate === 2.5" />
-        <fullHeart v-if="rate === 5" />
+        <halfHeart v-if="rate === 0.5" />
+        <fullHeart v-if="rate === 1" />
       </button>
     </div>
+
+    <!-- Display the total score -->
+    <div class="ml-4 text-xl font-bold">{{ totalScore }}</div>
+
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import emptyHeart from './icons/emptyHeart.vue';
 import fullHeart from './icons/fullHeart.vue';
 import halfHeart from './icons/halfHeart.vue';
@@ -23,21 +27,21 @@ const toggleHeart = (index) => {
   if (ratings.value[index] === 0) {
     // Make all previous hearts full
     for (let i = 0; i < index; i++) {
-      ratings.value[i] = 5;
+      ratings.value[i] = 1;
     }
-    ratings.value[index] = 2.5; // Make the current heart half-filled
+    ratings.value[index] = 0.5; // Make the current heart half-filled
     return;
   }
 
-  if (ratings.value[index] === 2.5) {
+  if (ratings.value[index] === 0.5) {
     // Make all previous hearts and the current one full
     for (let i = 0; i <= index; i++) {
-      ratings.value[i] = 5;
+      ratings.value[i] = 1;
     }
     return;
   }
 
-  if (ratings.value[index] === 5) {
+  if (ratings.value[index] === 1) {
     // Make the current heart and all subsequent hearts empty
     for (let i = index; i < ratings.value.length; i++) {
       ratings.value[i] = 0;
@@ -45,4 +49,10 @@ const toggleHeart = (index) => {
     return;
   }
 };
+
+// Calculate the total score based on the ratings array
+const totalScore = computed(() => {
+  return ratings.value.reduce((sum, rating) => sum + (rating)*2, 0);
+});
+
 </script>
